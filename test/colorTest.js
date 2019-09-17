88e238b6c17760746e98683cd8ba7072ed2d2ed6
Metwas <metwas.seup@gmail.com>
2019-09-17 20:15:57 +0100

    Added color testing

---
+++ test/colorTest.js
@@ -0,0 +1,12 @@
+const { utils, math, drawing } = require("../lib/broadleaf");
+const { table, test, printService } = require("./helpers/helper");
+const { log, header } = printService;
+const chalk = require("chalk");
+
+// testing initialization
+log(header("#### Color Testing ####"));
+
+const color = new drawing.color(255, 255, 255);
+color.setHex(0x010101);
+log(color.toString(true));
+test("Color test", color.red === 255);
