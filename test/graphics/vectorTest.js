7d86a06efe3e057593eb65f26910dfd26a1286c4
Metwas <metwas.seup@gmail.com>
2019-10-26 00:27:02 +0100

    Added vector testing

---
+++ test/graphics/vectorTest.js
@@ -0,0 +1,18 @@
+const { utils, math } = require("../../lib/broadleaf");
+const { table, test, printService } = require("../helpers/helper");
+const chalk = require("chalk");
+
+const vector2 = math.Vector2;
+const { log, header } = printService;
+
+// testing initialization
+log(header("#### Vector Testing ####"));
+const vel = new vector2(1, 2);
+const acc = new vector2(2, 1);
+
+console.log(`Magnitude: ${vel.magnitude()}`);
+console.log(`Heading: ${vel.heading()}`);
+// rotate
+vel.rotate(math.HALF_PI);
+console.log(`Heading: ${vel.heading()}`);
+
